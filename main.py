678ad98f4afdaab3c0e12b7516c8a046f216fa5c
Kartik Kaklotar <kartik.kaklotar@gmail.com>
2023-04-16 13:08:57 +0530

    Get Pets api added

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Pet(BaseModel):
+    id: int
+    name: str
+    status: str
+    tags: list[str] = []
+
+## Mock Data
+pets = [
+    Pet(id=1, name="Fluffy", status="available", tags=["cat"]),
+    Pet(id=2, name="Buddy", status="pending", tags=["dog"]),
+    Pet(id=3, name="Charlie", status="sold", tags=["dog"])
+]
+
 @app.get('/')
-async def root():
+def root():
     return {"Message":"Hello World!"}
+
+@app.get('/pets')
+def get_pets():
+    return pets
