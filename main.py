3771c086423bda24095889b82888e224552a85aa
Kartik Kaklotar <kartik.kaklotar@gmail.com>
2023-04-16 13:39:59 +0530

    post and put method add for pet api

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ class Pet(BaseModel):
     status: str
     tags: list[str] = []
 
+class NewPet(BaseModel):
+    name: str
+    status: str
+    tags: list[str] = []
+
+class UpdatePet(BaseModel):
+    name: str
+    status: str
+
 ## Mock Data
 pets = [
     Pet(id=1, name="Fluffy", status="available", tags=["cat"]),
@@ -23,3 +32,29 @@ def root():
 @app.get('/pets')
 def get_pets():
     return pets
+
+@app.get('/pet/{pet_id}')
+def get_pet_by_id(pet_id: int):
+    for p in pets:
+        if p.id == pet_id:
+            return p
+    return {'detail':'pet not found.'}
+
+@app.post('/pet')
+def add_pet(pet: NewPet):
+    pet_id = pets[-1].id + 1 if pets else 1
+    new_pet = Pet(id=pet_id, **pet.dict())
+    pets.append(new_pet)
+    return new_pet
+
+@app.put("/pet")
+def update_pet(pet: Pet):
+    pet_index = None
+    for i, p in enumerate(pets):
+        if p.id == pet.id:
+            pet_index = i
+            break
+    else:
+        return {"detail": "Pet not found."}
+    pets[pet_index] = pet
+    return pet
