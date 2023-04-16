c0652f263e0e69475a8694a10915766a92756a43
Kartik Kaklotar <kartik.kaklotar@gmail.com>
2023-04-16 13:47:32 +0530

    get pets by status and tags added

--- main.py
+++ main.py
@@ -33,13 +33,6 @@ def root():
 def get_pets():
     return pets
 
-@app.get('/pet/{pet_id}')
-def get_pet_by_id(pet_id: int):
-    for p in pets:
-        if p.id == pet_id:
-            return p
-    return {'detail':'pet not found.'}
-
 @app.post('/pet')
 def add_pet(pet: NewPet):
     pet_id = pets[-1].id + 1 if pets else 1
@@ -58,3 +51,23 @@ def update_pet(pet: Pet):
         return {"detail": "Pet not found."}
     pets[pet_index] = pet
     return pet
+
+@app.get('/pet/{pet_id}')
+def get_pet_by_id(pet_id: int):
+    for p in pets:
+        if p.id == pet_id:
+            return p
+    return {'detail':'pet not found.'}
+
+@app.get("/pet/findByStatus")
+def find_pets_by_status(status: str = "available"):
+    results = [p for p in pets if p.status == status]
+    return results
+
+@app.get("/pet/findByTags")
+def find_pets_by_tags(tags: list[str]):
+    results = []
+    for p in pets:
+        if set(tags) <= set(p.tags):
+            results.append(p)
+    return results
